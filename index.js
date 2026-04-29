54346a56e0303e8f9296d9deebb4060d0481574f
Bohdana2002 <bogdanaturcheniak@gmail.com>
2026-04-29 14:35:28 +0000

    Deploying to gh-pages from @ Bohdana2002/goit-js-hw-11@50b19956e96be4985d20ae39310f73cd5afc3517 🚀

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-import{a as u,S as f}from"./assets/vendor-95FE17ni.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&a(n)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function a(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();function m(i){return u.get(`https://pixabay.com/api/?key=55620354-ac6ed1e79a2ffd89ad6e48679&q=${i}&image_type=photo&orientation=horizontal&safesearch=true`).then(r=>r.data)}const p=new f(".gallery a",{captionsData:"alt",captionPosition:"bottom"});function d(i){const r=i.map(({largeImageURL:o,webformatURL:a,tags:e,likes:t,views:n,comments:l,downloads:c})=>` <li class="gallery-item">
-	<a class="gallery-link" href="${o}">
+import{S as f,a as m,i as p}from"./assets/vendor-JQ4mps58.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&o(i)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function o(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();const n=document.querySelector(".gallery"),l=document.querySelector(".js-loader"),y=new f(".gallery a",{captionsData:"alt",captionPosition:"bottom"});function g(a){const s=a.map(({largeImageURL:r,webformatURL:o,tags:e,likes:t,views:i,comments:u,downloads:d})=>` <li class="gallery-item">
+	<a class="gallery-link" href="${r}">
 		<img 
 		  class="gallery-image" 
-		  src="${a}"
+		  src="${o}"
 		  alt="${e}"
 		/>
 	</a>
-    <p> Likes "${t}"</p>
-    <p> Views "${n}"</p>
-    <p> Comments "${l}"</p>
-    <p> Downloads "${c}"</p>
-</li>`).join("");s.gallery.insertAdjacentHTML("beforeend",r),p.refresh()}const s={form:document.querySelector(".form"),gallery:document.querySelector(".gallery")};function y(i){i.preventDefault();const r=i.target.elements["search-text"].value.trim();r&&(s.gallery.innerHTML="",m(r).then(o=>{if(o.hits.length===0)return alert("Sorry, there are no images matching your search query. Please try again!");d(o.hits)}).catch(o=>console.log(o)))}s.form.addEventListener("submit",y);
+  <div class="field">
+  <div class="statistics">
+    <span class='titles'>Likes</span>
+    <span class='numbers'>${t}</span>
+  </div>
+  <div class="statistics">
+    <span class='titles'>Views</span>
+    <span class='numbers'>${i}</span>
+  </div>
+  <div class="statistics">
+    <span class='titles'>Comments</span>
+    <span class='numbers'>${u}</span>
+  </div>
+  <div class="statistics">
+    <span class='titles'>Downloads</span>
+    <span class='numbers'>${d}</span>
+  </div>
+</div>
+</li>`).join("");n.insertAdjacentHTML("beforeend",s),y.refresh()}function h(){n.innerHTML=""}function v(){l.classList.add("is-active")}function c(){l.classList.remove("is-active")}function b(a){return m.get(`https://pixabay.com/api/?key=55620354-ac6ed1e79a2ffd89ad6e48679&q=${a}&image_type=photo&orientation=horizontal&safesearch=true`).then(s=>s.data)}const L={form:document.querySelector(".form"),gallery:document.querySelector(".gallery"),preloader:document.querySelector(".js-loader")};function S(a){a.preventDefault();const s=a.target.elements["search-text"].value.trim();s&&(h(),v(),b(s).then(r=>{if(r.hits.length===0){p.error({position:"topRight",message:"Sorry, there are no images matching your search query. Please try again!"});return}g(r.hits)}).catch(r=>console.log(r)).finally(()=>{c()}))}c();L.form.addEventListener("submit",S);
 //# sourceMappingURL=index.js.map
